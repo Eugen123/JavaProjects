c4ceb26120ad69320707359a74d0d85f2f11bc5a
eugen.dragomir <dragomir.eugen@yahoo.com>
2017-01-04 19:16:38 +0200

    init client

--- PatientRegistrationSystem/PRSClient/src/sample/Controller.java
+++ PatientRegistrationSystem/PRSClient/src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import jdk.nashorn.internal.objects.annotations.Property;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by eugen.dragomir on 12/26/2016.
+ */
+public class Controller implements Initializable {
+
+    @FXML
+    private TextField regId;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        regId.setText("Registration Id");
+
+    }
 }
+
--- PatientRegistrationSystem/PRSClient/src/sample/Main.java
+++ PatientRegistrationSystem/PRSClient/src/sample/Main.java
@@ -1,19 +1,93 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main extends Application {
 
+    private static Socket socket;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        try {
+            String host = getPropertyValue("HOST");
+            String port = getPropertyValue("PORT");
+            InetAddress address = InetAddress.getByName(host);
+            socket = new Socket(address, Integer.parseInt(port));
+
+            while (true){
+                OutputStream os = socket.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+                BufferedWriter br = new BufferedWriter(osw);
+                BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+                String s = bufferRead.readLine();
+            }
+        } catch (Exception e){
+
+        }
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Patient Management System");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
+
+        addContent(primaryStage);
         primaryStage.show();
+
+
+    }
+
+    String getPropertyValue(String property)
+    {
+        try {
+
+            FileReader settings = new FileReader("C:\\Users\\eugen.dragomir\\IdeaProjects\\Test\\src\\sample\\settings");
+            BufferedReader bufferedReader = new BufferedReader(settings);
+            String line = null;
+            String[] array = null;
+            while ((line = bufferedReader.readLine()) != null){
+                array = line.split(property + "=");
+            }
+            settings.close();
+            bufferedReader.close();
+
+            return array[1];
+        }catch (Exception e){
+
+            Logger.getAnonymousLogger().log(Level.CONFIG,"Could not open settings file");
+            Logger.getAnonymousLogger().log(Level.CONFIG, e.getMessage());
+        }
+
+        return null;
+
+    }
+    void startClient(){
+        Socket client= null;
+        try {
+            client.connect(client.getLocalSocketAddress());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } {
+        };
+    }
+    public void addContent(Stage primaryStage){
+        Parent grid = (primaryStage.getScene().getRoot());
+
+
     }
 
 
